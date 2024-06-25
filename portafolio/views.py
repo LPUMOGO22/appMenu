99d2dbe287b5de44ce58870f7fdc44289f0c8693
lpumogo <luisenrique.moragomez@outlook.com>
2024-06-25 12:43:45 -0600

    Menu basico funcional

--- portafolio/views.py
+++ portafolio/views.py
@@ -1,5 +1,93 @@
-from django.http import HttpResponse
+from django.views.generic import TemplateView
+
+from .models import Categoria, Producto
+
+from django.views.generic import (
+    CreateView,
+    ListView,
+    UpdateView,
+    DeleteView,
+)
 
 
-def homePageView(request):
-    return HttpResponse("Hello, World!")
+class HomePageView(TemplateView):
+    template_name = "home.html"
+
+
+class CategoriaCreateView(CreateView):
+    model = Categoria
+    template_name = "categoria_create.html"
+    fields = [
+        "nombre",
+        "descripcion",
+    ]
+    success_url = "/"
+
+
+class CategoriaListView(ListView):
+    model = Categoria
+    template_name = "categoria_list.html"
+    context_object_name = "categorias"
+
+
+class CategoriaUpdateView(UpdateView):
+    model = Categoria
+    template_name = "categoria_update.html"
+    fields = [
+        "nombre",
+        "descripcion",
+    ]
+    success_url = "/"
+
+
+class CategoriaDeleteView(DeleteView):
+    model = Categoria
+    template_name = "categoria_delete.html"
+    success_url = "/"
+
+
+class ProductoCreateView(CreateView):
+    model = Producto
+    template_name = "producto_create.html"
+    fields = [
+        "nombre",
+        "descripcion",
+        "precio",
+        "disponibilidad",
+        "categoria",
+    ]
+    success_url = "/"
+
+
+class ProductoListView(ListView):
+    model = Producto
+    template_name = "producto_list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["desayuno_productos"] = Producto.objects.filter(
+            categoria__nombre="desayuno"
+        )
+        context["comida_productos"] = Producto.objects.filter(
+            categoria__nombre="comida"
+        )
+        return context
+
+
+class ProductoUpdateView(UpdateView):
+    model = Producto
+    template_name = "producto_update.html"
+    fields = [
+        "nombre",
+        "descripcion",
+        "precio",
+        "disponibilidad",
+        "categoria",
+    ]
+    success_url = "/"
+
+
+class ProductoDeleteView(DeleteView):
+    model = Producto
+    template_name = "producto_delete.html"
+    success_url = "/"
